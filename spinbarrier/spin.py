69721c46ae13ab0e30738fe3ac96d9628bb483e1
Kvík <martin.balaz@trojsten.sk>
2019-07-09 15:09:03 +0000

    Added spinbarrier

---
+++ spinbarrier/spin.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import numpy
+import scipy
+
+import argparse
+
+G = 6.674e-11
+M = 1e10
+rho = 1000
+omega = 0.0001
+
+def stabilize(profile):
+    
+
+def potential(surface):
+
+
+def main():
+    

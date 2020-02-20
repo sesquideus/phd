da5987726dce0858d90a5fe2433ae3129025aad8
Kvík <martin.balaz@trojsten.sk>
2020-02-20 15:20:46 +0000

    All in a day's work once more

--- 2-spring/colloquium/heights/heights.py
+++ 2-spring/colloquium/heights/heights.py
@@ -1,12 +1,27 @@
 import numpy as np
 import scipy
+import shutil
+import os
 from sklearn.neighbors import KernelDensity
 
 import matplotlib
 from matplotlib import pyplot as plt
 
+MALE_COUNT = 8000
+MALE_MEAN = 1.77
+MALE_SIGMA = 0.08
+FEMALE_COUNT = 6000
+FEMALE_MEAN = 1.625
+FEMALE_SIGMA = 0.06
+
+matplotlib.rc('font', **{
+    'family': 'Minion Pro',
+    'size': 22,
+})
+
+
 def generate():
-    return np.random.normal(1.625, 0.060, 6000), np.random.normal(1.770, 0.090, 8000)
+    return np.random.normal(FEMALE_MEAN, FEMALE_SIGMA, FEMALE_COUNT), np.random.normal(MALE_MEAN, MALE_SIGMA, MALE_COUNT)
 
 def histogram(data):
     return np.histogram(data, bins=np.linspace(1300, 2100, 80))
@@ -17,50 +32,111 @@ def gauss(data, c, mean, stddev):
 def double_gauss(data, c1, mean1, stddev1, c2, mean2, stddev2):
     return gauss(data, c1, mean1, stddev1) + gauss(data, c2, mean2, stddev2)
 
+female, male = generate()
+x = np.linspace(1.000, 2.500, 1500)
+bw = [1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 20, 30, 50, 100, 200, 300, 500, 1000, 1500, 3000, 5000, 10000]
+bandwidths = 10 ** np.linspace(-3.75, -0.5, 50)
+yo = double_gauss(x, MALE_COUNT, MALE_MEAN, MALE_SIGMA, FEMALE_COUNT, FEMALE_MEAN, FEMALE_SIGMA)
 
-def main():
-    female, male = generate()
-    stacked = np.hstack((female, male))
-#    plt.hist(stacked, bins=80)
-#    plt.show()
-#    plt.hist(female, stacked=False, bins=80, color=(1, 0.4, 0, 0.6))
-#    plt.hist(male, stacked=False, bins=80, color=(0, 0, 0.7, 0.6))
-    x = np.linspace(0.500, 2.500, 1600)
-
-    matplotlib.rc('font', **{
-        'family': 'Minion Pro',
-        'size': 22,
-    })
-
+def kde_frames(data):
+    shutil.rmtree('kde', ignore_errors=True)
+    os.makedirs('kde')
     i = 0
-    bandwidths = 10**np.linspace(-3.75, -0.75, 100)
     mises = []
+
     for h in bandwidths:
-        kde = KernelDensity(kernel='gaussian', bandwidth=h).fit(stacked[:, np.newaxis])
-        y = 14000 * np.exp(kde.score_samples(x[:, np.newaxis]))
-        yo = double_gauss(x, 6000, 1.625, 0.060, 8000, 1.770, 0.090)
+        kde = KernelDensity(kernel='gaussian', bandwidth=h).fit(data[:, np.newaxis])
+        y = (FEMALE_COUNT + MALE_COUNT) * np.exp(kde.score_samples(x[:, np.newaxis]))
+
         result = scipy.integrate.trapz((yo - y)**2, x)
         mises.append(result)
 
         plt.figure(figsize=(16, 10))
         plt.fill_between(x, y, 0, color=(0, 0, 0.7, 0.5))
+        plt.fill_between(x, (y-yo)**2 / 1e3, 0, color=(0.7, 0.3, 0, 0.2))
         plt.plot(x, yo)
         plt.xlim(1, 2.5)
         plt.ylim(0, 80000)
         plt.tight_layout(rect=(0, 0, 1, 1))
         plt.title(f'Bandwidth {h:.6f}, MISE {result:.0f}')
-        plt.savefig(f'{i:03d}.png')
+        plt.savefig(f'kde/{i:03d}.png')
         plt.close()
         print(f"{i}")
         i = i + 1
 
+    return mises
+
+def plot_mises(mises):
     plt.figure(figsize=(16, 10))
     plt.xlim(0, 0.2)
     plt.yscale('log')
     plt.plot(bandwidths, mises)
     plt.tight_layout(rect=(0, 0, 1, 1))
-    plt.savefig('mise.png')
+    plt.savefig('kde/mise.png')
     plt.close()
+
+
+def hist_frames(data):
+    shutil.rmtree('hist', ignore_errors=True)
+    os.makedirs('hist')
+    i = 0
+    mises = []
+
+    for h in bw:
+        plt.figure(figsize=(16, 10))
+        bins, edges = np.histogram(data, bins=np.linspace(1, 2.5, h + 1), density=True)
+
+        plt.bar(edges[:-1], (MALE_COUNT + FEMALE_COUNT) * bins, width=1.5 / h, align='edge', color=(0.7, 0, 0.5, 0.5))
+
+        result = 1#scipy.integrate.trapz((yo - y)**2, x)
+#        mises.append(result)
+
+        plt.plot(x, yo)
+        plt.xlim(1, 2.5)
+        plt.ylim(0, 80000)
+        plt.tight_layout(rect=(0, 0, 1, 1))
+        plt.title(f'Bin width {1.5 / h:.6f}, MISE {result:.0f}')
+        plt.savefig(f'hist/{i:03d}.png')
+        plt.close()
+        print(f"{i}")
+        i = i + 1
+
+    return mises
+
+def fit(data):
+    xh = np.linspace(1, 2.5, 151)
+    bins, edges = np.histogram(data, bins=xh, density=True)
+    popt, pcov = scipy.optimize.curve_fit(double_gauss, xh[:-1], bins, ftol=1e-12)
+    males = popt[0] * (MALE_COUNT + FEMALE_COUNT)
+    females = popt[3] * (MALE_COUNT + FEMALE_COUNT)
+    male_mean = popt[1]
+    female_mean = popt[4]
+    male_sigma = popt[2]
+    female_sigma = popt[5]
+
+    plt.figure(figsize=(16, 10))
+    plt.xlim(1, 2.5)
+    plt.ylim(0, 60000)
+    plt.bar(edges[:-1], (MALE_COUNT + FEMALE_COUNT) * bins, width=1.5 / 150, align='edge', color=(0.7, 0, 0.5, 0.5))
+    plt.plot(x, yo)
+    plt.plot(x, double_gauss(x, males, male_mean, male_sigma, females, female_mean, female_sigma))
+    plt.plot(x, gauss(x, males, male_mean, male_sigma))
+    plt.plot(x, gauss(x, females, female_mean, female_sigma))
+    plt.tight_layout(rect=(0, 0, 1, 1))
+    plt.title(f'Males: {males:.0f}, {male_mean:.3f} m, females: {females:.0f}, {female_mean:.3f} m')
+    plt.show()
+
+
+def main():
+    stacked = np.hstack((female, male))
+
+    #mises = kde_frames(stacked)
+    #plot_mises(mises)
+
+    #mises = hist_frames(stacked)
+    #plot_mises(mises)
+
+    fit(stacked)
 
 main()
 
